60d46aa88a675466c399b18ca199f9efe9133afd
Robbie <100627896+robin-haxx@users.noreply.github.com>
2024-03-13 21:04:51 +1300

    full bleed + style + colour

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -18,17 +18,25 @@ function draw_one_frame(cur_frac) {
 	
 	//translate(width / 2, height / 2);
 	
-	let unitsOnField = 12;
+	let unitsOnField = 16;
 	let unitSize = width/unitsOnField;
-	let spacing = width / unitsOnField - 1;
+	let spacing = width / unitsOnField ;
 
 	let noiseNumber;
-	// let darker = color("#004237"); //monochrome colour to lerp with white.
+	let darker = color("#004237"); //monochrome colour to lerp with white.
 	let white = color("#ffffff");
-	let darker = color("#000000");
+	// let darker = color("#000000");
 
-	for (let x = 1; x < (width / spacing)-1; x++){
-		for (let y =1; y+1 < height / spacing; y++ ){
+
+
+	// for (let x = 1; x < (width / spacing) -1; x++){
+	// 	for (let y = 1; y < (height / spacing)-1; y++)
+	// }
+
+
+
+	for (let x =0; x <= (width / spacing); x++){
+		for (let y =0; y <= height / spacing; y++ ){
 		//	noiseDetail(10,.9);
 		noiseDetail(2);
 			
@@ -45,13 +53,13 @@ function draw_one_frame(cur_frac) {
 			zero_to_zero = map(cur_frac,0,1,1,0);
 			
 			if(cur_frac < 0.5){
-				rect(spacing*x, spacing*y, cur_frac*unitSize/2, unitSize);
-				rect(spacing*x, spacing*y, unitSize, cur_frac*unitSize/2);
+				rect(spacing*x, spacing*y, cur_frac*unitSize*2, unitSize);
+				rect(spacing*x, spacing*y, unitSize, cur_frac*unitSize*2);
 			} else {
-				rect(spacing*x, spacing*y, zero_to_zero*unitSize/2, unitSize);
-				rect(spacing*x, spacing*y, unitSize, zero_to_zero*unitSize/2);
+				rect(spacing*x, spacing*y, zero_to_zero*unitSize*2, unitSize);
+				rect(spacing*x, spacing*y, unitSize, zero_to_zero*unitSize*2);
 			}
-			rect(spacing*x, spacing*y, unitSize/1.8, unitSize/1.8);
+			rect(spacing*x, spacing*y, unitSize/1.5, unitSize/1.8);
 			fill(lerpColor(darker, white, noiseColour));
 			if(noiseColour < 0.6){
 				fill(0);
@@ -67,8 +75,8 @@ function draw_one_frame(cur_frac) {
 		
 		// rect(width/2,height/2,unitSize/3*unitsOnField,unitSize*unitsOnField,unitSize/2);
 		 fill(lerpColor(white, darker, noiseColour));
-		 if(noiseColour > 0.7){
-		 	fill(255,5);
+		 if(noiseColour > 0.5){
+		 	fill(255,50);
 		 }
 		ellipse(width/2,height/2,width/2.4,height/3.5);
 		image(img,width/4,height/4,width/2,height/2);
